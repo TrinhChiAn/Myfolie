201c2134ecbf9a2a033204ac01b6b2c18e36b0ab
TrinhChiAn <antrinh060@gmail.com>
2025-06-11 13:53:59 +0700

    fix

--- myfolio_fe/src/config/constants.js
+++ myfolio_fe/src/config/constants.js
@@ -1,2 +1,2 @@
-export const API_URL = 'http://45.77.172.27:8080/api';
-export const BASE_URL = 'http://45.77.172.27:8080'; 
+export const API_URL = 'http://66.42.51.94:8080/api';
+export const BASE_URL = 'http://66.42.51.94:8080'; 
